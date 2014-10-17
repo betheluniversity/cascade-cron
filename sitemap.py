0233612146bca6b57f092576e17156f7dafe019e
Eric Jameson <e-jameson@bethel.edu>
2014-10-17 11:41:20 -0500

    append to file

--- sitemap.py
+++ sitemap.py
@@ -1,19 +1,18 @@
 __author__ = 'ejc84332'
-import time
 
 #python
 import logging
+import time
+import datetime
+
 #ext
 import crython
 
 
-logging.basicConfig(filename='/opt/cascade-cron/cascade-cron/logs/stdout.log', filemod='w', level=logging.INFO)
-
-
 @crython.job(second=range(0,60,10))
 def foo():
-    with open('/var/www/staging/public/_testing/jmo/cron.txt', 'w') as f:
-        f.write("Print info every 10 seconds to test supervisord")
+    with open('/var/www/staging/public/_testing/jmo/cron.txt', 'a') as f:
+        f.write("%s -- Print info every 10 seconds to test supervisord" % datetime.now())
 
 
 if __name__ == '__main__':
