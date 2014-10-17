3a3be7b02beadd77c9a0c941f16a67c17ec9db42
Eric Jameson <e-jameson@bethel.edu>
2014-10-17 11:33:43 -0500

    test writing to file

--- sitemap.py
+++ sitemap.py
@@ -8,10 +8,11 @@ import crython
 
 logging.basicConfig(filename='/opt/cascade-cron/cascade-cron/logs/stdout.log', filemod='w', level=logging.INFO)
 
+
 @crython.job(second=range(0,60,10))
 def foo():
-    logging.info("Print info every 10 seconds to test supervisord")
-    logging.warning("Print warning every 10 seconds to test supervisord")
+    with open('/var/www/staging/public/_testing/jmo/cron.txt', 'w') as f:
+        f.write("Print info every 10 seconds to test supervisord")
 
 
 if __name__ == '__main__':
