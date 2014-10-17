e439c24ff9e60bd017fd1c45899acffed7bfbec3
Eric Jameson <e-jameson@bethel.edu>
2014-10-17 11:16:39 -0500

    use logging

--- sitemap.py
+++ sitemap.py
@@ -1,14 +1,14 @@
 __author__ = 'ejc84332'
 
 #python
-
+import logging
 #ext
 import crython
 
 
 @crython.job(second=range(0,60,10))
 def foo():
-    print "Print stuff every 10 seconds to test supervisord"
+    logging.info("Print stuff every 10 seconds to test supervisord")
 
 
 if __name__ == '__main__':
