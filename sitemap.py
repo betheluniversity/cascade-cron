8a7f13e0ff86baee79d075b49ef22a28452203f2
Eric Jameson <e-jameson@bethel.edu>
2014-10-17 11:17:43 -0500

    does info print?

--- sitemap.py
+++ sitemap.py
@@ -8,7 +8,8 @@ import crython
 
 @crython.job(second=range(0,60,10))
 def foo():
-    logging.info("Print stuff every 10 seconds to test supervisord")
+    logging.info("Print info every 10 seconds to test supervisord")
+    logging.warning("Print warning every 10 seconds to test supervisord")
 
 
 if __name__ == '__main__':
