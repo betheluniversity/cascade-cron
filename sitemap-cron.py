794a988bd62ffda03dd6371a388ea3c80c306e37
Caleb Schwarze <ces55739@bethel.edu>
2016-11-01 09:22:55 -0500

    Removed tinker's sync all

--- sitemap-cron.py
+++ sitemap-cron.py
@@ -71,14 +71,7 @@ def sitemap_cron():
 def get_adult_programs():
     r = requests.get("http://programs.bethel.edu/adultprograms/sync-all/30/send")
     print "got %s" % r.text
-
-
-# Fire once a minute.
-@crython.job(expr='@daily')
-def get_school_and_depts():
-    r = requests.get("http://tinker.bethel.edu/sync/all")
-    print "got %s" % r.text
-
+    
 
 @crython.job(expr='@daily')
 def load_humans_txt():
