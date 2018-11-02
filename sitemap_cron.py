e158254183ccd2646af1e37236d2b1e4caffbfd6
c-schwarze <ces55739@bethel.edu>
2018-11-02 18:14:00 -0500

    added sitemap sftp

--- sitemap_cron.py
+++ sitemap_cron.py
@@ -27,29 +27,6 @@ def sitemap_cron():
         sitemap.sitemap()
     except:
         client.captureException()
-    # Now that sitemap is done generating take care of a few things.
-    # 1. fix up robots.txt and site-index.xml (remove system-region)
-    # robots.txt and sitemap-index.xml published at midnight,
-    with open(config.ROBOTS_FILE) as file:
-        lines = file.read().splitlines()
-
-    with open(config.ROBOTS_PRODUCTION_FILE, 'w') as file:
-        file.write("\n".join(lines))
-
-    # Now sitemap-index.xml
-    with open(config.SITEMAP_INDEX_FILE) as file:
-        lines = file.read().splitlines()
-
-    # 2. Move the new sitemap to replace the old one. Can't replace the old one right away
-    # because it is a generator, so it would be incomplete while it runs.
-    with open(config.SITEMAP_INDEX_PRODUCTION_FILE, 'w') as file:
-        file.write("\n".join(lines))
-
-    with open(config.SITEMAP_FILE) as file:
-        lines = file.read().splitlines()
-
-    with open(config.SITEMAP_PRODUCTION_FILE, 'w') as file:
-        file.write("\n".join(lines))
 
 
 @crython.job(expr='@daily')
