4f4ea7c86a6346245700a2a016aa22b1918a4986
Bailey Middendorf <bam95899@bethel.edu>
2019-03-12 09:29:03 -0500

    Added in xml syntax check

--- sitemap_cron.py
+++ sitemap_cron.py
@@ -1,11 +1,20 @@
 # local
+import sys
 import sitemap
 import config
 from raven import Client
 from github_connection import GH
+from xml.sax.handler import ContentHandler
+from xml.sax import make_parser
 
 
 client = Client(config.SENTRY_URL)
+
+
+def parse_file(filename):
+    parser = make_parser()
+    parser.setContentHandler(ContentHandler())
+    parser.parse(filename)
 
 
 def sitemap_cron():
@@ -27,12 +36,18 @@ def sitemap_cron():
     except:
         client.captureException()
 
-    # # 2. Move the new sitemap to replace the old one. Can't replace the old one right away
-    # # because it is a generator, so it would be incomplete while it runs.
-    # with open(config.SITEMAP_INDEX_PRODUCTION_FILE, 'w') as file:
-    #     file.write("\n".join(lines))
+    # 2. Move the new sitemap to replace the old one. Can't replace the old one right away
+    # because it is a generator, so it would be incomplete while it runs.
+    with open(config.SITEMAP_INDEX_PRODUCTION_FILE, 'w') as file:
+        file.write("\n".join(lines))
     with open(config.SITEMAP_FILE) as file:
         lines = file.read().splitlines()
+        try:
+            parse_file(file)
+        except Exception, e:
+            # Throws an error and exits so file isn't written to with invalid xml
+            client.captureMessage(e)
+            sys.exit(0)
     with open(config.SITEMAP_PRODUCTION_FILE, 'w') as file:
         file.write("\n".join(lines))
 
